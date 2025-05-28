352b445c8476031356347910d5fcd5fdf3c1e55a
zxy <2361380031@qq.com>
2025-05-28 13:51:18 +0800

    修改Mixins命名规则

--- src/main/java/com/husheng/fastman/mixin/FastFurnaceToAbstractFurnaceBlockEntityMixin.java
+++ src/main/java/com/husheng/fastman/mixin/FastFurnaceToAbstractFurnaceBlockEntityMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractFurnaceBlockEntity.class)
-public abstract class FastFurnaceBlockEntityMixin {
+public abstract class FastFurnaceToAbstractFurnaceBlockEntityMixin {
     
     private final static int fast_times = 20;
     
